8ce1d4a7b0b84a59e195142292ba7f271e92c902
Klara Bryntesson <k_bryntesson@hotmail.com>
2023-05-26 10:52:03 +0200

    changes for deploy-test

--- backend/index.ts
+++ backend/index.ts
@@ -26,7 +26,7 @@ app.use(cors());
 
 app.use(express.urlencoded({ extended: false }));
 
-app.use(express.static(path.join(path.resolve(), "public")));
+app.use(express.static(path.join(__dirname, "public")));
 app.use("/images", express.static(path.join(__dirname, "publicimages")));
 
 app.get(
